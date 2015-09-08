a4d1f838ccc57fe6675aa744b75aedb7bed3ee12
Haskel Ash <haskelash@hotmail.com>
2015-09-08 09:58:36 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shoes::Application.config.secret_token = '0e61b2e9cbd9c37964bebadfd090779e6e669ac1a058e162d0dd28517c7ee922ac7baa9e8b20fe6c53338500098c00ae67c47a131b6c84325545d11bcdbbf100'
